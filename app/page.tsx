3ee1b4668a396e16548df2cade5743412a6f65f3
nntcuong <thecuongyt3htk@gmail.com>
2024-03-04 23:20:20 +0700

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">
-
-          Login with Google
-        </button>
-      </div>
+    <div>
+      E-Shop
     </div>
   );
 }
