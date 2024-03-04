7f32376f9b9a084ed6ac539a36bc6ab4ed93adde
nntcuong <thecuongyt3htk@gmail.com>
2024-03-05 00:12:27 +0700

    commit

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,6 +1,34 @@
-const NavBar=()=>{
-    return<div>
-        NavBar
+import Link from "next/link";
+import Container from "../Container";
+const NavBar = () => {
+    return <div
+        className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm"
+    >
+        <div className="py-4 border-b-[1px]">
+            <Container>
+                <div className="
+                flex
+                item-center
+                justify-between
+                gap-3
+               md:gap-0
+                ">
+                    <Link href="/"> Mercedes</Link>
+                
+                <div className="hidden md:block">
+                    Search
+                </div>
+                <div className="">
+                    <div>
+                        CartCount
+                    </div>
+                    <div>
+                        UserMenu
+                    </div>
+                </div>
+                </div>
+            </Container>
+        </div>
     </div>
 }
 export default NavBar;
